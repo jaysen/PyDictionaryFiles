38e65c2071653cf67f6a54e7b1608f8ade29141d
Jaysen Naidoo <jaysen@gmail.com>
2014-06-28 12:53:54 +0200

    v0.3 - working Dictionary Files in dictfiles.py. Updated fileutils.py

---
+++ filedict.py
@@ -0,0 +1,230 @@
+""" dictfiles.py
+    version 0.3
+
+    Reads dictionary values to and from specially formatted text files.
+    Preserves comments and non key:value pairs
+
+    by Jaysen Naidoo
+    2014.06.28
+
+    ChangeLog:
+    2014.06.28: v0.3 starts with fixes:
+        - leaves comments and non key pairs alone.
+        - only replaces a line if it contains a keypair and the value has changed
+    2006.08.21: fixed update - to properly update the file when a keyword is changed
+
+    TODO: Handle Key:Values in the following formats:
+            - [key:value]
+            - [key=value]
+    TODO: handle multi-line key:value pairs (?)
+    TODO: Allow the reading of multiple key:value formats
+
+    TODO: change dictionary to component instead of parent (?)
+
+
+
+    # Copyright (C) 2014 Jaysen
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+import os
+from UserDict import UserDict
+import fileutils as fu
+
+
+DEFAULTSEPERATOR = ':'
+
+
+class FileDict(UserDict):
+
+    """ A class that reads and writes dictionary key:values from text files
+        Ignores & Preserves comments and non key:value pairs
+    """
+
+    filename = './test.txt'
+    seperator = DEFAULTSEPERATOR
+    raiseErrors = False
+
+    def __init__(self, fname=None, sep=DEFAULTSEPERATOR):
+        """ inits the class - by checking filename and reading in dict values
+            arguments:
+                fname = filename to work with
+                sep         : (optional) the separator used in the file (default = ':')
+                raiseErrors : (optional) whether or not to raise exceptions (default = False)
+        """
+        UserDict.__init__(self)
+        self.filename = fname
+        self.seperator = sep
+        self.fileRead()
+        
+    def fileRead(self):
+        """ reads from file into FileDict
+            returns 1 if file exists - else returns 0
+        """
+        if os.path.exists(self.filename):
+            for line in open(self.filename, 'r'):
+                splitline = line.split(self.seperator)
+                if len(splitline) > 1:
+                    self.data[splitline[0].strip()] = splitline[1].strip()
+            return 1
+        # doesn't exist - so create empty file:
+        else:
+            f = open(self.filename, 'w')
+            f.close()
+        return 0
+
+    def fileUpdate(self, keepbackups=True):
+        """ updates the file with the contents of the dictionary
+            Preserves comments and non key:value pairs
+
+            returns 1 it if succeeds, otherwise 0
+        """
+        if os.path.exists(self.filename):
+            fu.backup(self.filename)
+
+        tempdict = self.data
+        templines = []
+        for line in fu.readlines(self.filename):
+            templine = line
+            splitline = templine.split(self.seperator)
+            if len(splitline) > 1:
+                tempkey = splitline[0].strip()
+                tempval = splitline[1].strip()
+                if tempkey in self.data:
+                    if tempdict[tempkey] == tempval:
+                        templines.append(line.strip())
+                    else:
+                        templines.append('%-20s : %s'.strip() % (tempkey, tempdict[tempkey]))
+                    # del existing pairs from tempdict - so you dont repeat them when u append tempdict
+                    del tempdict[tempkey]
+            else:
+                templines.append(line.strip())
+
+        for k in sorted(tempdict.iterkeys()):
+            templines.append('%-20s : %s'.strip() % (k, tempdict[k]))
+        fu.writelines(self.filename, templines, True)
+        if not keepbackups:
+            fu.delete(self.filename + '.bak')
+
+# -----------------------------------------------------------------------------
+    def getValue(self, key, raw=False):
+        """ Returns the value associated with the key
+            Returns NULL if not found
+            If Raw=true: get from file.
+        """
+        if key in self:
+            return self.data[key]
+
+    def printDict(self):
+        for k, v in sorted(fd.iteritems()):
+            print '%-20s : %s' % (k, v)
+
+    def printDictFile(self):
+        print("")
+        if os.path.exists(self.filename):
+            #log('print fd file contents:')
+            for l in (fu.readlines(self.filename)):
+                print l,
+
+
+def log(string):
+    """ debug/log output """
+    print ("******    %s" % string)
+
+
+if __name__ == "__main__":
+
+    testfile = './test2.txt'
+    # TEMP DEBUG - DELETE BEFORE:
+    # fu.delete(testfile)
+
+    fd = FileDict(testfile)
+    fd.fileRead()
+
+    try:
+        if ('read-count' in fd.data.keys() and int(fd.data['read-count']) > 5):
+            log("DELETING!!!")
+            fu.delete(testfile)
+            fd.data.clear()
+    except:
+        log("readcount failed!!!!!")
+
+    if not os.path.exists(testfile):
+        log("create a new dict file")
+        newlines = ["TESTING DICTFILES.PY",
+                    "- A Script to read and handle Key/Value Pairs embedded files with normal text",
+                    "",
+                    "read-count :0:random comment after this",
+                    "greetings friendly tester.",
+                    "",
+                    "c:0",
+                    "DD:09",
+                    "varA:1:random comment after this",
+                    "testWord :       the WORD : a comment ... and another",
+                    "and YET another comment",
+                    "",
+                    "then the END OF FREE TEXT!!!!!!!"]
+        fu.writelines(testfile, newlines, True)
+        fd.fileRead()
+
+    # log("===================================")
+    # log("print the dict file first time!")
+    # fd.printDictFile()
+
+    # log("read dict from the file")
+    # fd.fileRead()
+
+    log("===================================")
+    log("Print the dictionary:")
+    fd.printDict()
+
+    log("===================================")
+    log("Then change the dict in code ...")
+
+    fd.data['testWord'] = 'the WORD'
+    fd['testSentence'] = 'a whole SENTENCE'
+
+    varA, varB = '1', '5'
+    readcount = '0'
+    try:
+        varA = str(int(fd.data['varA']) + 1)
+        varB = str(int(fd.data['varB']) + 5)
+        readcount = str(int(fd.data['read-count']) + 1)
+    except:
+        pass
+    finally:
+        fd.data['varA'] = varA
+        fd.data['varB'] = varB
+        fd.data['read-count'] = readcount
+
+    fd['DD'] = '10'
+    fd['DE'] = '10'
+    fd['DD'] = '11'
+
+    # log("print the dictionary BEFORE update:")
+    # fd.printDict()
+
+    log("===================================")
+    log('now update file from new fd.data:')
+    fd.fileUpdate()
+
+    log("print the dictionary AFTER update:")
+    fd.printDict()
+
+    print("")
+    log('then reprint the FileDict file:')
+    fd.printDictFile()
---
+++ fileutils.py
@@ -0,0 +1,230 @@
+""" fileutils.py
+    v1.5
+    This module contains convenience functions for working with files and paths.
+
+    Jaysen Naidoo : 2006.01.16
+    released subject to GPL, except for functions by Michael Foord # Copyright Michael Foord 2004 (BSD)
+"""
+
+import os
+import sys
+
+
+# ---file convenience functions---#000000#FFFFFF---------------------------------
+
+def editfile(fname, func, keepbackups=True):
+    """ Given a filename fname, and a function, editFunc
+            performs editFunc on a list of lines from file fname
+            and writes to file fname
+
+            Returns 1 if succeeds, else 0
+    """
+    try:
+        if not os.path.isfile(fname):
+            writelines(fname, [''])
+        renamefile(fname, fname + '.bak')
+        flines = readlines(fname + '.bak')
+        try:
+            newlines = func(flines)
+            if newlines is None:
+                # TODO: check return type !!!!!
+                print 'Error in editfile : the user-specified edit function MUST return a list of strings'
+        except Exception, funcErr:
+            print 'Error in editfile : the user-specified edit function %s failed \n' % func
+            print '%s' % funcErr
+            return 0
+        writelines(fname, newlines)
+        if not keepbackups:
+            os.remove(fname + '.bak')
+        return 1
+    except Exception, editfileErr:
+        print 'couldnt edit file %s' % fname
+        print '%s' % editfileErr
+        return 0
+
+
+def renamefile(old, new, deleteExisting='safe'):
+    """ renames file old to new
+            Needs to be called with deleteexisting == 'unsafe' to rename if new filename already exists
+    """
+    if os.path.exists(new):
+        if deleteExisting == 'unsafe':
+            os.remove(new)
+    os.rename(old, new)
+
+
+def getlinecount(fname):
+    """ Returns number of lines in fname"""
+    return len(readlines(fname))
+
+
+def removelines(fname, L1, L2, bak=True):
+    """deletes lines L1 to L2 from fname"""
+    editfile(fname, lambda lines: lines[:L1] + lines[L2:], bak)
+
+
+def addlinenums(fname):
+    """ adds line numbers to textfile fname eg. '1. this is the first line' """
+    def adnums(lines):
+        i = 1
+        out = []
+        for li in lines:
+            out.append('%s. %s' % (i, li))
+            i += 1
+        return out
+    editfile(fname, adnums)
+
+
+# ---Functions from pathutils---#00FF00#040404-----------------------------------
+# the following functions by Michael Foord :
+# Copyright Michael Foord 2004
+# Released subject to the BSD License
+# Please see http://www.voidspace.org.uk/documents/BSD-LICENSE.txt
+
+
+# ---Functions to read and write files in text and binary mode.---#000000#FFFFFF-
+
+def readlines(filename, createifmissing=True):
+    """Passed a filename, it reads it, and returns a list of lines. (Read in text mode)"""
+    if os.path.exists(filename):
+        filehandle = open(filename, 'r')
+        outfile = filehandle.readlines()
+        filehandle.close()
+        return outfile
+    elif createifmissing:
+        filehandle = open(filename, 'w')
+        filehandle.close()
+        return []
+    return None
+
+
+def writelines(filename, infile, newline=False):
+    """
+    Given a filename and a list of lines it writes the file. (In text mode)
+    If newline is True (default is False) it adds a newline to each line.
+    Overwrites the existing file if the file exists.
+    If the file does not exist, creates a new file.
+    """
+    filehandle = open(filename, 'w+')
+    if newline:
+        infile = [line + '\n' for line in infile]
+    filehandle.writelines(infile)
+    filehandle.close()
+
+
+def readbinary(filename):
+    """Given a filename, read a file in binary mode. It returns a single string."""
+    filehandle = open(filename, 'rb')
+    thisfile = filehandle.read()
+    filehandle.close()
+    return thisfile
+
+
+def writebinary(filename, infile):
+    """Given a filename and a string, write the file in binary mode. """
+    filehandle = open(filename, 'wb')
+    filehandle.write(infile)
+    filehandle.close()
+
+
+def readfile(filename):
+    """Given a filename, read a file in text mode. It returns a single string."""
+    filehandle = open(filename, 'r')
+    outfile = filehandle.read()
+    filehandle.close()
+    return outfile
+
+
+def writefile(filename, infile):
+    """Given a filename and a string, write the file in text mode."""
+    filehandle = open(filename, 'w+')
+    filehandle.write(infile)
+    filehandle.close()
+
+
+def printfile(filename):
+    """Given a filename print the contents"""
+    for l in readlines(filename):
+        print l,
+
+# ------#000000#FFFFFF-----------------------------------------------------------
+
+
+def fullcopy(src, dst):
+    """
+    Copy file from src to dst.
+
+    If the dst directory doesn't exist, we will attempt to create it using makedirs.
+    """
+    import shutil
+    if not os.path.isdir(os.path.dirname(dst)):
+        os.makedirs(os.path.dirname(dst))
+    shutil.copy(src, dst)
+
+
+def delete(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+
+def backup(filename):
+    import shutil
+    bakfile = filename + '.bak'
+    delete(bakfile)
+    # os.rename(filename,bakfile)
+    shutil.copy(filename, filename + '.bak')
+    return bakfile
+
+
+# ------#000000#FFFFFF-----------------------------------------------------------
+def import_path(fullpath, strict=True):
+    """
+    Import a file from the full path. Allows you to import from anywhere,
+    something __import__ does not do.
+
+    If strict is True (the default), raise an ImportError if the module
+    is found in the "wrong" directory.
+
+    Taken from firedrop2_ by `Hans Nowak`_
+
+    .. _firedrop2: http://www.voidspace.org.uk/python/firedrop2/
+    .. _Hans Nowak: http://zephyrfalcon.org
+    """
+    path, filename = os.path.split(fullpath)
+    filename, ext = os.path.splitext(filename)
+    sys.path.insert(0, path)
+    try:
+        module = __import__(filename)
+    except ImportError:
+        del sys.path[0]
+        raise
+    del sys.path[0]
+    #
+    if strict:
+        path = os.path.split(module.__file__)[0]
+        # FIXME: doesn't *startswith* allow room for errors ?
+        if not fullpath.startswith(path):
+            raise ImportError, "Module '%s' found, but not in '%s'" % (
+                    filename, fullpath)
+    #
+    return module
+
+
+# ---main()---#FFFFFF#004080------------------------------------------------------
+if __name__ == "__main__":
+
+    f = './test.txt'
+    tl = ['why god ,why',
+          '++from tombo',
+          '....',
+          'whyyyyyyyyy   ....',
+          'my name is sam',
+          '++end of from tombo',
+          ' ',
+          '',
+          '      stilll heeeeerrrreeee.....     '
+          ]
+    writelines(f, tl, True)
+    addlinenums(f)
+
+    printfile(f)
